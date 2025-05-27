1695898e0753df73aae14b5c71ffab5c147e337d
franchisepartner <partnerbuddy17@gmail.com>
2025-05-27 15:28:55 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
+// pages/_app.tsx
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navbar from '../components/Navbar'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <main className="pt-16">
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
