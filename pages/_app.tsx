c4254871b4e7afc93abdd2a49bbe72748c8e9111
franchisepartner <partnerbuddy17@gmail.com>
2025-06-01 21:24:32 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
-// File: pages/_app.tsx
-
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Navbar from '../components/Navbar'
+// pages/_app.tsx
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Navbar from '../components/Navbar';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const isHome = router.pathname === '/';
+
   return (
     <>
       <Navbar />
-      <main className="pt-0">
+      <main className={isHome ? '' : 'pt-16'}>
         <Component {...pageProps} />
       </main>
     </>
-  )
+  );
 }
