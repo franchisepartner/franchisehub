c204fc0871f9f40b263e9f982c83b3b20e667771
franchisepartner <partnerbuddy17@gmail.com>
2025-06-10 16:50:09 +0800

    Update sitemap.xml.tsx

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,61 +1,43 @@
-// pages/sitemap.xml.tsx
-
 import { GetServerSideProps } from 'next';
 import { supabase } from '../lib/supabaseClient';
 
 const SITE_URL = 'https://franchisenusantara.com';
 
-function generateSiteMap({
-  franchises,
-  blogs,
-  threads,
-}: {
-  franchises: { slug: string }[];
-  blogs: { slug: string }[];
-  threads: { slug: string }[];
-}) {
+function generateSiteMap({ franchises, blogs, threads }: { franchises: { slug: string }[]; blogs: { slug: string }[]; threads: { slug: string }[] }) {
   return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
   <url>
     <loc>${SITE_URL}/</loc>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>
-
   ${franchises
-    .map(
-      ({ slug }) => `
+    .map(({ slug }) => `
   <url>
     <loc>${SITE_URL}/listing/${slug}</loc>
     <changefreq>weekly</changefreq>
     <priority>0.9</priority>
-  </url>`
-    )
+  </url>
+  `)
     .join('')}
-
   ${blogs
-    .map(
-      ({ slug }) => `
+    .map(({ slug }) => `
   <url>
     <loc>${SITE_URL}/blog/${slug}</loc>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
-  </url>`
-    )
+  </url>
+  `)
     .join('')}
-
   ${threads
-    .map(
-      ({ slug }) => `
+    .map(({ slug }) => `
   <url>
     <loc>${SITE_URL}/forum/${slug}</loc>
     <changefreq>weekly</changefreq>
     <priority>0.6</priority>
-  </url>`
-    )
+  </url>
+  `)
     .join('')}
-
 </urlset>`;
 }
 
@@ -66,7 +48,7 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
 
   const franchises = franchisesRaw ?? [];
   const blogs = blogsRaw ?? [];
-  const threads = (threadsRaw ?? []).map((t) => ({ slug: t.id }));
+  const threads = (threadsRaw ?? []).map(t => ({ slug: t.id }));
 
   const sitemap = generateSiteMap({ franchises, blogs, threads });
 
@@ -77,7 +59,6 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   return { props: {} };
 };
 
-// HARUS export default function kosong, agar tidak render HTML
 export default function Sitemap() {
   return null;
 }
