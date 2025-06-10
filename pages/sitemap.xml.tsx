84a907bdf28c04547be716f5872d1eb30bbd3e65
franchisepartner <partnerbuddy17@gmail.com>
2025-06-10 16:42:48 +0800

    Update sitemap.xml.tsx

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,14 +1,18 @@
 // /pages/sitemap.xml.tsx
 
 import { GetServerSideProps } from 'next';
-import { supabase } from '../lib/supabaseClient'; // <-- PASTIKAN path benar!
+import { supabase } from '../lib/supabaseClient';
 
 const SITE_URL = 'https://franchisenusantara.com';
 
-function generateSiteMap({ franchises, blogs, threads }: {
-  franchises: { slug: string }[],
-  blogs: { slug: string }[],
-  threads: { id: string }[]
+function generateSiteMap({
+  franchises,
+  blogs,
+  threads,
+}: {
+  franchises: { slug: string }[];
+  blogs: { slug: string }[];
+  threads: { slug: string }[];
 }) {
   return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -17,35 +21,47 @@ function generateSiteMap({ franchises, blogs, threads }: {
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>
-  <!-- Tambah url lain jika mau -->
-  ${franchises.map(({ slug }) => `
+  ${franchises
+    .map(
+      ({ slug }) => `
   <url>
     <loc>${SITE_URL}/listing/${slug}</loc>
     <changefreq>weekly</changefreq>
     <priority>0.9</priority>
-  </url>
-  `).join('')}
-  ${blogs.map(({ slug }) => `
+  </url>`
+    )
+    .join('')}
+  ${blogs
+    .map(
+      ({ slug }) => `
   <url>
     <loc>${SITE_URL}/blog/${slug}</loc>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
-  </url>
-  `).join('')}
-  ${threads.map(({ id }) => `
+  </url>`
+    )
+    .join('')}
+  ${threads
+    .map(
+      ({ slug }) => `
   <url>
-    <loc>${SITE_URL}/forum/${id}</loc>
+    <loc>${SITE_URL}/forum/${slug}</loc>
     <changefreq>weekly</changefreq>
     <priority>0.6</priority>
-  </url>
-  `).join('')}
+  </url>`
+    )
+    .join('')}
 </urlset>`;
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  const { data: franchises = [] } = await supabase.from('franchises').select('slug');
-  const { data: blogs = [] } = await supabase.from('blogs').select('slug');
-  const { data: threads = [] } = await supabase.from('forum_threads').select('id');
+  const { data: franchisesRaw } = await supabase.from('franchises').select('slug');
+  const { data: blogsRaw } = await supabase.from('blogs').select('slug');
+  const { data: threadsRaw } = await supabase.from('forum_threads').select('id');
+
+  const franchises = franchisesRaw ?? [];
+  const blogs = blogsRaw ?? [];
+  const threads = (threadsRaw ?? []).map((t) => ({ slug: t.id }));
 
   const sitemap = generateSiteMap({ franchises, blogs, threads });
 
@@ -53,10 +69,9 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   res.write(sitemap);
   res.end();
 
-  return { props: {} }; // <-- HARUS ADA!
+  return { props: {} };
 };
 
-// HARUS DEFAULT EXPORT FUNCTION KOSONG!
 export default function Sitemap() {
   return null;
 }
