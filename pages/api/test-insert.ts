05cda99438fc6ac1d860fa1a1754aaf096575465
franchisepartner <partnerbuddy17@gmail.com>
2025-06-16 19:38:36 +0800

    Update test-insert.ts

--- pages/api/test-insert.ts
+++ pages/api/test-insert.ts
@@ -1,6 +1,7 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { createClient } from '@supabase/supabase-js';
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const supabaseAdmin = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!
